5cb95bf3477c9be96818b50cbdb2a105f0ed071b
g3tbusy <1@gmail.com>
2024-02-07 15:00:54 +0300

    .

--- mdk0103-java-core/src/Game/Army.java
+++ mdk0103-java-core/src/Game/Army.java
@@ -2,5 +2,15 @@ package Game;
 
 public class Army
 {
+    private String type;
+    private int hp;
+    private int gold;
+    private int damage;
 
+    public Army(String type, int hp, int gold, int damage) {
+        this.type = type;
+        this.hp = hp;
+        this.gold = gold;
+        this.damage = damage;
+    }
 }
---
+++ mdk0103-java-core/src/Game/Enemies.java
@@ -0,0 +1,9 @@
+package Game;
+
+public class Enemies
+{
+    private String type;
+    private int hp;
+    private int gold;
+    private int damage;
+}
