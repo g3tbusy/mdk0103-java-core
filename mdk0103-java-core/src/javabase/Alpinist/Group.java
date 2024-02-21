165708501803e6e43118e9a52cb7445c08346159
g3tbusy <1@gmail.com>
2024-02-21 12:50:38 +0300

    as

--- mdk0103-java-core/src/javabase/Alpinist/Group.java
+++ mdk0103-java-core/src/javabase/Alpinist/Group.java
@@ -2,47 +2,8 @@ package javabase.Alpinist;
 
 public class Group extends Alpinist
 {
-    private int currentIndex;
-    private Alpinist[] Alpinist;
-    private boolean isJoinOpened;
-
-    public Group(String name, int age, String address, int groupsize)
+    public Group(String name, int age, String address)
     {
         super(name, age, address);
-        this.Alpinist = new Alpinist[groupsize];
-        this.isJoinOpened = false;
-        this.currentIndex = 0;
     }
-
-    // метод для добавления альпиниста в группу
-    public void addAlpinist(Alpinist alpinist)
-    {
-        if (isJoinOpened && currentIndex < Alpinist.length)
-        {
-            Alpinist[currentIndex++] = alpinist;  // добавляем альпиниста в массив и увеличиваем текущий индекс
-            System.out.println(alpinist.getName() + " добавлен в группу для восхождения на " + Mountain.getName());
-        }
-        else if (!isJoinOpened)
-        {
-            System.out.println("Набор в группу на восхождение на " + Mountain.getName() + " не открыт");
-        }
-        else
-        {
-            System.out.println("Группа полная для восхождения на " + Mountain.getName());
-        }
-    }
-
-    // метод показывает сколько альпинистов пойдут на гору
-    public void printAlpinists()
-    {
-        System.out.println("Альпинисты в группе для восхождения на " + Mountain.getName() + ":");
-        for (Alpinist alpinist : Alpinist)
-        {
-            if (alpinist != null)
-            {
-                System.out.println(alpinist.getName());
-            }
-        }
-    }
-
 }
--- mdk0103-java-core/src/javabase/Alpinist/Main.java
+++ mdk0103-java-core/src/javabase/Alpinist/Main.java
@@ -41,7 +41,7 @@ public class Main
                 System.out.println("");
 
             case 3:
-                addAlpinist(Alpinist alpinist);
+
         }
     }
 }
